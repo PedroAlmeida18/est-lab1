c4591068b3df71aa982f6628eb9d48a9b74f7024
PedroAlmeida18 <109418292+PedroAlmeida18@users.noreply.github.com>
2022-12-06 20:56:00 +0000

    Atividade classe dlete,inseireprocuraretc

--- ListaArray.java
+++ ListaArray.java
@@ -1,21 +1,52 @@
-public class ListaArray implements EstruturaDeDados{
+import java.util.Scanner;
 
+public class ListaArray implements EstruturaDeDados{
+   private  int [] elementos;
+   private  int contador ;
+   Scanner leitor = new Scanner(System.in);
+    public ListaArray(){
+       elementos = new int[1000];
+       contador = 0;
+        
+    }
+ 
     @Override
-    public boolean insert(int chave) {
-        // TODO Auto-generated method stub
-        return false;
+    public boolean insert(int chave ) {
+        if (contador < this.elementos.length) {
+            this.elementos[contador]  = chave;
+            contador +=1;
+            return true ;
+        } else {
+            return false;
+        }
+        
     }
 
     @Override
     public boolean delete(int chave) {
-        // TODO Auto-generated method stub
+        for (int i = 0; i <elementos.length;i++){
+            if(chave == elementos[i]){
+                elementos[i] = 0;
+                for ( int j = i; j < contador -1; j++){
+                    elementos[j]= elementos[j+1];
+
+                }
+                contador -= 1;
+                return true;
+            }
+        } 
         return false;
     }
 
     @Override
     public boolean search(int chave) {
-        // TODO Auto-generated method stub
-        return false;
+        for( int i = 0; i< elementos.length;i++){
+            if ( chave == elementos[i]){
+               return true;
+            }
+            
+        } return false;
+       
     }
 
     @Override
@@ -44,5 +75,10 @@ public class ListaArray implements EstruturaDeDados{
 
     public static void main(String[] args) {
         ListaArray r = new ListaArray();
+        r.insert(445);
+        System.out.println(r.search(445));
+        r.search(455);
+        r.delete(445);
+        System.out.println(r.search(445));
     }
 }
